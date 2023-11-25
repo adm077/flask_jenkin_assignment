d9fb0125a59905b1c4d57fd7217e9ec8c7dbc6f8
adm077 <139608052+adm077@users.noreply.github.com>
2023-11-25 15:53:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/', method=['GET'])
 def helloworld():
     return 'Hellow World'
 
-if _name_ == "_main_":
+if __name__ == "_main_":
     app.run(port=3000, debug=True)
